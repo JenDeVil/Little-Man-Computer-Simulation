02f065ab032ccfd5b9e9d97994488b82b8bed9c8
Jennifer-Delilah-Vil Universe <128077606+JenDeVil@users.noreply.github.com>
2023-04-10 15:38:03 +0100

    Deleted redundant ProgramCounter.py file
    The contents of the ProgramCounter.py file were moved into the new Registers.py file for easier management and so this file is now redundant

--- ProgramCounter.py
+++
@@ -1,9 +0,0 @@
-from MemLoc import MemLoc
-
-
-class ProgramCounter(MemLoc):
-    def __init__(self):
-        super().__init__(0, 99, 0)
-
-    def incr(self):
-        self.setValue(self.getValue() + 1)
